d56182e099ed81944d6dcece6b1e94b4080ced92
Vashisht Madhavan <vashisht@uber.com>
2019-06-05 13:19:28 -0700

    making ppo inherit from a2c

--- algos/ppo/run_atari.py
+++ algos/ppo/run_atari.py
@@ -8,18 +8,18 @@ from common.models import AtariPolicy
 def parse_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--env', type=str, default='PongNoFrameskip-v4', help='environment name')
-	parser.add_argument('--threads', type=int, default=8, help='number of parallel threads to collect data')
-	parser.add_argument('--seed', type=int, default=1234, help='seed for experiment')
-	parser.add_argument('--timesteps', type=int, default=int(1e6), help='number of timesteps to train for')
+	parser.add_argument('--threads', type=int, default=16, help='number of parallel threads to collect data')
+	parser.add_argument('--seed', type=int, default=0, help='seed for experiment')
+	parser.add_argument('--timesteps', type=int, default=int(10e6), help='number of timesteps to train for')
 	parser.add_argument('--outdir', type=str, default='a2c_debug/', help='location of saved output from training')
 	parser.add_argument('--gamma', type=float, default=0.99, help='discount factor')
-	parser.add_argument('--lr', type=float, default=7e-4, help='learning rate for training the policy')
+	parser.add_argument('--lr', type=float, default=2.5e-4, help='learning rate for training the policy')
 	parser.add_argument('--gpu', type=str, default='0', help='which GPU to use. If no GPU use -1')
 
 	# PPO specific params
-	parser.add_argument('--n_step', type=int, default=32, help='number of steps to collect before update')
+	parser.add_argument('--n_step', type=int, default=128, help='number of steps to collect before update')
 	parser.add_argument('--tau', type=float, default=0.95, help='discount factor for GAE')
-	parser.add_argument('--vf_coef', type=float, default=0.5, help='coef for value function loss')
+	parser.add_argument('--vf_coef', type=float, default=0.25, help='coef for value function loss')
 	parser.add_argument('--ent_coef', type=float, default=0.01, help='coef for entropy loss')
 	parser.add_argument('--surr_batches', type=int, default=4, help='number of surrogate batch updates')
 	parser.add_argument('--surr_epochs', type=int, default=4, help='number of surrogate epochs')
@@ -36,7 +36,7 @@ def main(args):
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 	# Train PPO
-	ppo = PPO(env, AtariQNetwork, device, args)
+	ppo = PPO(env, AtariPolicy, device, args)
 	ppo.train()
 
 if __name__ == "__main__":
