44241b95d23c71b3610e4c9874c1dcc7155d9c66
Vashisht Madhavan <vashisht@uber.com>
2019-06-21 11:20:18 -0700

    adding run mujoco script to a2c

--- algos/a2c/run_mujoco.py
+++ algos/a2c/run_mujoco.py
@@ -1,7 +1,7 @@
 import torch
 import os
 import argparse
-from algos.ppo.ppo import PPO
+from algos.a2c.a2c import A2C
 from common.utils import make_mujoco_env
 from common.models import MLPPolicy
 
@@ -9,20 +9,17 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default='HalfCheetah-v2', help='environment name')
     parser.add_argument('--timesteps', type=int, default=int(1e6), help='number of timesteps to train for')
-    parser.add_argument('--outdir', type=str, default='ppo_mujoco_debug/', help='location of saved output from training')
+    parser.add_argument('--outdir', type=str, default='a2c_mujoco_debug/', help='location of saved output from training')
     parser.add_argument('--gamma', type=float, default=0.99, help='discount factor')
     parser.add_argument('--lr', type=float, default=3e-4, help='learning rate for training the policy')
     parser.add_argument('--log_iters', type=int, default=int(1e4), help='log at this frequency of timesteps')
     parser.add_argument('--gpu', type=str, default='0', help='which GPU to use. If no GPU use -1')
 
-    # PPO specific params
+    # A2C specific params
     parser.add_argument('--n_step', type=int, default=2048, help='number of steps to collect before update')
     parser.add_argument('--tau', type=float, default=0.95, help='discount factor for GAE')
     parser.add_argument('--vf_coef', type=float, default=0.25, help='coef for value function loss')
     parser.add_argument('--ent_coef', type=float, default=0.0, help='coef for entropy loss')
-    parser.add_argument('--surr_batches', type=int, default=32, help='number of surrogate batch updates')
-    parser.add_argument('--surr_epochs', type=int, default=10, help='number of surrogate epochs')
-    parser.add_argument('--clip_frac', type=float, default=0.2, help='clip fraction for advantage ratio')
     return parser.parse_args()
 
 def main(args):
@@ -33,9 +30,9 @@ def main(args):
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # Train PPO
-    ppo = PPO(env, MLPPolicy, device, args)
-    ppo.train()
+    # Train A2C
+    a2c = A2C(env, MLPPolicy, device, args)
+    a2c.train()
 
 if __name__ == "__main__":
     args = parse_args()
